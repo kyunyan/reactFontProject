31659faf35db804c96ad9189bfce66a41b5f514a
leegeonwan <gwlee@exciform.com>
2021-03-08 20:11:56 +0900

    cors 설정 및 백앤드 따로 호출

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Suspense } from "react";
-import {BrowserRouter, Route, Switch } from "react-router-dom";
+import {Route, Switch } from "react-router-dom";
 
 import UserListPage from "./components/views/userListPage/userListPage.js";
 
@@ -7,12 +7,10 @@ function App(){
   return (
     <Suspense fallback={(<div>Loading...</div>)}>
       <div style={{ paddingTop: '69px', minHeight: 'calc(100vh - 80px)' }}>
-       <BrowserRouter>
         <Switch>
            <Route exact path="/" component={UserListPage} />
 
           </Switch>
-       </BrowserRouter>
       </div>
     </Suspense>
   )
--- src/components/views/userListPage/userListPage.js
+++ src/components/views/userListPage/userListPage.js
@@ -1,17 +1,55 @@
-import React, {useEffect} from "react";
-import Antd, {} from 'antd'
+import React, {useEffect, useState} from "react";
+import axios from 'axios'
+import { Row, Col, Divider } from 'antd';
 
 function UserListPage(){
     //const dispatch = useDispatch();
+    const [UserList, setUserList] = useState([]);
 
-    //useEffect(() => {
-    //    console.log("test");
-   // })
+    const url = "http://localhost:8080"
+    useEffect(() => {
+        axios.get(url+`/user/findUserList`)
+        .then(response => { 
+            setUserList(response.data);
+        })
+        .catch(err => alert(err))
+    },[]);
+
+    const gridMain = UserList.map((userInfo) => {
+        return( 
+          <Row key={userInfo.id} >
+            <Col span={4} order={3}>
+                {userInfo.userID}
+            </Col>
+            <Col span={4} order={2}>
+                {userInfo.userNm}
+            </Col>
+            <Col span={5} order={1}>
+                {userInfo.userAge}
+            </Col>
+         </Row>
+        )
+    })
 
     return (
-        <div>
-            userList Page
-        </div>
+        <div style={{width: '100%', padding: '3rem 4rem'}}>
+        <div style={{display: 'flex', justifyContent: 'center' }}>
+            <Divider orientation="left">List</Divider>
+        </div> 
+            <Row>
+                <Col span={4} order={3} >
+                    유저아이디
+                </Col>
+                <Col span={4} order={2} >
+                    유저이름
+                </Col>
+                <Col span={4} order={1}>
+                    나이
+                </Col>
+            </Row>
+        <br />
+        {gridMain}
+       </div>
     )
 }
 
